e536bfa4749394b7948d12ee0bc89768d24b2918
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-01-01 22:35:58 +0800

    js fundamental - PART 1

---
+++ fundamental-1/script.js
@@ -0,0 +1,16 @@
+let js = 'fikri';
+if(js === 'amazing') alert('JS is FUN');
+
+console.log(40 + 40 - 24 + 10);
+
+let firstName = "Mathilda";
+
+console.log(firstName);
+
+if(js === 'amazing') console.log(firstName);
+
+while(js === 'amazing') {
+    for (let i = 0; i < 10; i++) {
+      console.log(firstName + i);
+    }
+}
