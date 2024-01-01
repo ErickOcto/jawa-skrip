658d6c7a7f7a7374ada58e2b20d6b80bb2e21b7c
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-01-01 22:58:55 +0800

    end of day 1 learn JAWASKRIP

--- fundamental-1/script.js
+++ fundamental-1/script.js
@@ -1,3 +1,5 @@
+// Variable and value
+
 let js = 'fikri';
 if(js === 'amazing') alert('JS is FUN');
 
@@ -14,3 +16,23 @@ while(js === 'amazing') {
       console.log(firstName + i);
     }
 }
+
+// Datatypes
+
+let javascriptIsFun = false;
+console.log(typeof firstName);
+console.log(typeof 23);
+console.log(typeof "This is quick!");
+console.log(typeof true);
+
+let javascript;
+javascript = "yes sir!";
+console.log(typeof javascript);
+
+let erick = true;
+erick = "ini string";
+
+console.log(typeof erick);
+
+console.log(typeof null);
+// 
