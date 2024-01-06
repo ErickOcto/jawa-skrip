be2ec58feda63523caf84a015ed682292a06e963
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-01-07 01:14:53 +0800

    Functions is Easy

--- fundamental-2/script2.js
+++ fundamental-2/script2.js
@@ -1,8 +1,26 @@
 //activating strict mdoe
 'use strict';
 
-let hasDriversLicenses = false;
+// let hasDriversLicenses = false;
 
-const passTest = true;
-if(passTest) hasDriversLicenses = true;
-if(hasDriversLicenses) console.log(hasDriversLicenses);
+// const passTest = true;
+// if(passTest) hasDriversLicenses = true;
+// if(hasDriversLicenses) console.log(hasDriversLicenses);
+
+//Functions
+
+// function logger(){
+//     console.log('My Name Is Erick');
+// }
+
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges){
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//     return console.log(`Juice with ${apples} apples and ${oranges} oranges`);
+// }
+
+// fruitProcessor(5, 1);
+// fruitProcessor(10, 10);
+//console.log(appleJuice);
