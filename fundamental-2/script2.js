b66561b7e73c992c03a3b08bb8dbcb75fa14b3d4
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-01-07 02:58:13 +0800

    Functions Calling Other Functions

--- fundamental-2/script2.js
+++ fundamental-2/script2.js
@@ -62,3 +62,18 @@
 
 // console.log(yearsUntilRetirement(1991, 'Jonas'));
 // console.log(yearsUntilRetirement(2005, 'Shifa'));
+
+//Functions Calling Other Functions
+function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges){
+    const applePieces = cutFruitPieces(apples);
+    const orangesPieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} of apples and ${orangesPieces} of oranges`;
+    return juice;
+}
+
+console.log(fruitProcessor(2,4));
