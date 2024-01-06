2fc7dc42ba9a8ec390d41fc2a3ef397d01501705
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-01-06 11:59:47 +0800

    let. const and var

--- fundamental-1/script.js
+++ fundamental-1/script.js
@@ -1,38 +1,39 @@
 // Variable and value
 
-let js = 'fikri';
-if(js === 'amazing') alert('JS is FUN');
+// let js = 'fikri';
+// if(js === 'amazing') alert('JS is FUN');
 
-console.log(40 + 40 - 24 + 10);
+// console.log(40 + 40 - 24 + 10);
 
-let firstName = "Mathilda";
+// let firstName = "Mathilda";
 
-console.log(firstName);
+// console.log(firstName);
 
-if(js === 'amazing') console.log(firstName);
+// if(js === 'amazing') console.log(firstName);
 
-while(js === 'amazing') {
-    for (let i = 0; i < 10; i++) {
-      console.log(firstName + i);
-    }
-}
+// while(js === 'amazing') {
+//     for (let i = 0; i < 10; i++) {
+//       console.log(firstName + i);
+//     }
+// }
 
 // Datatypes
 
-let javascriptIsFun = false;
-console.log(typeof firstName);
-console.log(typeof 23);
-console.log(typeof "This is quick!");
-console.log(typeof true);
+// let javascriptIsFun = false;
+// console.log(typeof firstName);
+// console.log(typeof 23);
+// console.log(typeof "This is quick!");
+// console.log(typeof true);
 
-let javascript;
-javascript = "yes sir!";
-console.log(typeof javascript);
+// let javascript;
+// javascript = "yes sir!";
+// console.log(typeof javascript);
 
-let erick = true;
-erick = "ini string";
+// let erick = true;
+// erick = "ini string";
 
-console.log(typeof erick);
+// console.log(typeof erick);
 
-console.log(typeof null);
-// 
+// console.log(typeof null);
+
+
