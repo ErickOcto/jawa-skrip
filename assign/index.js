6fd5d209d9553d49f834285618a3dbeea46fb3db
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-01-06 12:25:34 +0800

    first challanges

---
+++ assign/index.js
@@ -0,0 +1,12 @@
+markWeight = 78;
+markTall = 1.69 ** 2;
+
+johnWeight = 92;
+johnTall = 1.95 ** 2;
+
+data1 = markWeight / markTall > johnWeight / johnTall;
+
+console.log(markWeight, markTall, johnWeight, johnTall);
+let markHigherBMI = data1;
+
+console.log(markHigherBMI);
