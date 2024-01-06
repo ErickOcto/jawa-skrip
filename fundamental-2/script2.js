b9ffea2501b58b23b2ec95491c3534b30c6342ef
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-01-07 00:44:38 +0800

    activating strict mode

---
+++ fundamental-2/script2.js
@@ -0,0 +1,8 @@
+//activating strict mdoe
+'use strict';
+
+let hasDriversLicenses = false;
+
+const passTest = true;
+if(passTest) hasDriversLicenses = true;
+if(hasDriversLicenses) console.log(hasDriversLicenses);
