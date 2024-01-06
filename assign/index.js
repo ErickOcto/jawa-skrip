b97fde2c3b08d92e4355ad9f52b92a864c97d57d
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-01-06 21:01:15 +0800

    assign 4 is VERY VERY EZZZ

--- assign/index.js
+++ assign/index.js
@@ -27,3 +27,16 @@ if (scoreDolphins > scoreKoalas){
 } else {
     console.log("Both win the trophy");
 }
+
+//assign 4
+const bill = 275;
+
+/* Write your code below. Good luck! 🙂 */
+
+const tip = bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
+
+let totalValue = bill + tip;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${totalValue}`
+);
